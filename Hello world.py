e62b28f19ef7d83bc074535c9841f9bd1c5502e7
Life2death <vikram.panmand@gmail.com>
2023-06-23 16:07:09 +0530

    Test

---
+++ Hello world.py
@@ -0,0 +1,13 @@
+#print(len(input("whats your name ")))
+#name = input("whats your name")
+#print =(name)
+#print(" hello user good evening ")
+#City = input("which city did you grew into")
+#pet = input ("which pet do u like ")
+#print (City + pet)
+
+two_digit_number = input("Type a two digit number: ")
+f_digit = two_digit_number[0]
+s_digit = two_digit_number[1]
+result = int(f_digit) + int(s_digit)
+print (result)
