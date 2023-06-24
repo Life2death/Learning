eb135ada98641875ef184a4629b2637d9c200a50
Life2death <vikram.panmand@gmail.com>
2023-06-24 13:32:53 +0530

    3

---
+++ day 3.py
@@ -0,0 +1,7 @@
+print ("welcome to the roller coster")
+height = int (input("whstd your height"))
+
+if height> 120:
+    print ("you can ride the roller coaster")
+else:
+    print("please grow up ")
