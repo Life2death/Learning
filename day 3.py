f1d3f68ffe4967045a67dafa6f674d6e546a160d
Life2death <vikram.panmand@gmail.com>
2023-08-21 17:36:28 +0530

    non

--- day 3.py
+++ day 3.py
@@ -1,7 +1,15 @@
 print ("welcome to the roller coster")
 height = int (input("whstd your height"))
+age = int(input("whats your age "))
 
-if height> 120:
+if height >= 120:
     print ("you can ride the roller coaster")
+    if age < 12:
+         print ("Please pay 5")
+    elif age<=18:
+        print ("Please pay 7")
+    else:
+        print ("Please pay 12")
+
 else:
     print("please grow up ")
